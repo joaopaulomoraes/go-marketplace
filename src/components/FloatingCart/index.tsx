5d6a922b612a11086c251212c9f9caa118c7e23d
João Paulo Moraes <joaopaulomoraes@outlook.com>
2020-07-29 22:55:27 -0300

    chore: Finish methods

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useMemo } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
 
@@ -15,24 +15,29 @@ import formatValue from '../../utils/formatValue';
 
 import { useCart } from '../../hooks/cart';
 
-// Calculo do total
-// Navegação no clique do TouchableHighlight
-
 const FloatingCart: React.FC = () => {
   const { products } = useCart();
 
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const productsTotal = products.reduce((accumulator, product) => {
+      const productsSubTotal = product.price * product.quantity;
 
-    return formatValue(0);
+      return productsSubTotal + accumulator;
+    }, 0);
+
+    return formatValue(productsTotal);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const productsTotal = products.reduce((accumulator, product) => {
+      const productsQuantity = product.quantity;
 
-    return 0;
+      return productsQuantity + accumulator;
+    }, 0);
+
+    return productsTotal;
   }, [products]);
 
   return (
